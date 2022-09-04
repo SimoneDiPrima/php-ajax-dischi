6622d493e3bcf7bd64be28ac4f054ff09580323f
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-04 18:08:56 +0200

    versione 2 da rivedere perche non fa chiamata Api corretta

---
+++ api/index.php
@@ -0,0 +1,4 @@
+<?php require '../versione1/includes/discs.php'; 
+header('Content-Type: application/json');
+json_encode($discs);
+?>
---
+++ versione2/main.js
@@ -0,0 +1,13 @@
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el:'#root',
+    data:{
+        discs :[]
+    },
+    mounted(){
+        axios.get('http://localhost:8080/api/index.php').then((res)=>{
+            this.discs = res.data;
+        })
+    }
+})
