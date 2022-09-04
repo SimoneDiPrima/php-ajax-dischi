bba7daba9800bfa103d1626fc57b99011c14322e
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-04 16:25:10 +0200

    aggiunta cartella style e modifiche layout versione uno

--- index.php
+++ index.php
@@ -1,81 +1,34 @@
-<?php
-
-$discs = [
-    [
-        'title' => 'New Jersey',
-        'author' => 'Bon Jovi',
-        'year' => 1988,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
-        'genre' => 'Rock'
-    ],
-    [
-        'title' => 'Live at Wembley 86',
-        'author' => 'Queen',
-        'year' => 1992,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
-        'genre' => 'Pop'
-    ],
-    [
-        'title' => 'Ten\'s Summoner\'s Tales',
-        'author' => 'Sting',
-        'year' => 1993,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
-        'genre' => 'Pop'
-    ],
-    [
-        'title' => 'Steve Gadd band',
-        'author' => 'Steve Gadd Band',
-        'year' => 2018,
-        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
-        'genre' => 'Jazz'
-    ],
-    [
-        'title' => 'Brave new World',
-        'author' => 'Iron Maiden',
-        'year' => 2000,
-        'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
-        'genre' => 'Metal'
-    ],
-    [
-        'title' => 'One more car, one more rider',
-        'author' => 'Eric Clapton',
-        'year' => 2002,
-        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
-        'genre' => 'Rock'
-    ]
-];
-?>
+<?php include '<includes/discs.php'?>
 <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- CSS only -->
-        
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
         <link rel="stylesheet" href="./css/style.css">
-        <title>Document</title>
+        <title>SPOTIFY</title>
     </head>
     <body>
         <div id="MainDisks">
             <div class="container py-5">
-                <div class="Disks-list">
-                    <div class="text-white singleDisk">
-                        <ul class="col-lg-3">
-                            <li><?php foreach($discs as $disc) : ?>
-                                <h2><?php echo $disc['title'] ?></h2>
-                                <h3><?php echo $disc['author'] ?></h3>
-                                <h4><?php echo $disc['year'] ?></h4>
-                                <img src="<?php echo $disc['poster']?>" alt="<?php $disc['title']?>" />
-                                <h6><?php echo $disc['genre'] ?></h6>
-                            </li>
-                                <? endforeach; ?>
-                        </ul>
-                    </div> 
+                <div class="Disks-list d-flex">
+                    <?php if(isset($discs)) : ?>
+                        <div class="text-white d-flex flex-row">
+                            <ul class="">
+                                <li><?php foreach($discs as $disc) : ?>
+                                    <h2><?php echo $disc['title'] ?></h2>
+                                    <h3><?php echo $disc['author'] ?></h3>
+                                    <h4><?php echo $disc['year'] ?></h4>
+                                    <img src="<?php echo $disc['poster']?>" alt="<?php $disc['title']?>" />
+                                    <h6><?php echo $disc['genre'] ?></h6>
+                                </li>
+                                    <? endforeach; ?>
+                            </ul>
+                        </div> 
+                    <?php endif; ?>
                 </div>
             </div>
-
         </div>
     </body>
     </html>
